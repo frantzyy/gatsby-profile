cc3023773aa0c558cd5c8c1b92a1ecb399a6b6cd
Chris Frantz <frantzyy@yahoo.com>
2021-04-07 22:33:18 -0400

    upgrade google analytics to gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -216,10 +216,24 @@ module.exports = {
   plugins: [
     // Make sure this plugin is first in the array of plugins
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-WNKFZ0WFPV",
-        head: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-WNKFZ0WFPV"
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true
+        },
       },
     },
     `gatsby-plugin-react-helmet`,
